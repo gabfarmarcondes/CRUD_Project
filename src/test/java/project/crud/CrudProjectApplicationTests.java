7b51f2e4fe5a25243c8320553006beccbec477ba
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-07-04 22:57:06 -0300

    First Commit - Conexão com o BD

---
+++ src/test/java/project/crud/CrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
