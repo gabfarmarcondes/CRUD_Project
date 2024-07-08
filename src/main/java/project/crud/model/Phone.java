f07db59f1b7207ecfbb949151dc5b569f71669d5
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2024-07-08 11:14:07 -0300

    Criando os atributos de User

---
+++ src/main/java/project/crud/model/Phone.java
@@ -0,0 +1,16 @@
+package project.crud.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class Phone {
+    private int DDI;
+    private int DDD;
+    private String number;
+
+}
--- src/main/java/project/crud/model/User.java
+++ src/main/java/project/crud/model/User.java
@@ -1,6 +1,7 @@
 package project.crud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,4 +18,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    private String password;
+
+    @Email
+    private String email;
+
+    @Embedded
+    private Phone phoneNumber;
+
 }
