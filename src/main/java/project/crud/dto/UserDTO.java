ae8d47db3fa70e06f2addce28dbddb0516da0643
gabfarmarcondes <gabriel.fmarcondes@hotmail.com>
2025-04-03 18:34:01 -0300

    Reconfiguração da Entidade e do seu Repository

--- src/main/java/project/crud/dto/UserDTO.java
+++ src/main/java/project/crud/dto/UserDTO.java
@@ -1,35 +1,15 @@
 package project.crud.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.UniqueElements;
 import project.crud.model.Phone;
 
 public record UserDTO(
   Long id,
-
-  @NotBlank
   String username,
-
-  @NotBlank
   String firstName,
-
-  @NotBlank
   String lastName,
-
-  @NotBlank
-  @UniqueElements
   String email,
-
-  @UniqueElements
-  @NotBlank
   String password,
-
   String cpf,
-
-  @NotNull
   Phone phoneNumber
-
 )
-{
-}
+{ }
--- src/main/java/project/crud/model/User.java
+++ src/main/java/project/crud/model/User.java
@@ -2,11 +2,14 @@ package project.crud.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 import project.crud.dto.UserDTO;
 
 
-@Entity(name = "user")
+@Entity
+@Table(name = "users")
 @Setter
 @Getter
 @AllArgsConstructor
@@ -18,26 +21,35 @@ public class User{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "username", unique = true, nullable = false, length = 20)
+    @NotBlank(message = "User's username can not be blank")
     private String username;
 
-    @Column(nullable = false)
+    @Column(name = "firstName", nullable = false, length = 50)
+    @NotBlank(message = "User's first name can not be blank")
     private String firstName;
 
-    @Column(nullable = false)
+    @Column(name = "lastName", nullable = false, length = 50)
+    @NotBlank(message = "User's last name can not be blank")
     private String lastName;
 
-    @Column(nullable = false)
-    private String password;
-
-    @Email
-    @Column(unique = true, nullable = false)
+    @Email(message = "Invalid E-mail")
+    @Column(name = "user_email", unique = true, nullable = false)
+    @NotBlank(message = "User's e-mail can not be blank")
     private String email;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "user_password", nullable = false)
+    @NotBlank(message = "User's password can not be blank")
+    private String password;
+
+    @CPF(message = "Invalid CPF")
+    @Column(name = "user_cpf", unique = true, nullable = false)
+    @NotBlank(message = "User's CPF can not be blank")
     private String cpf;
 
     @Embedded
+    @Column(name = "user_phone", unique = true, nullable = false)
+    @NotBlank(message = "Users phone can not be blank")
     private Phone phoneNumber;
 
     public User(UserDTO userDTO){
@@ -45,10 +57,10 @@ public class User{
         this.username = userDTO.username();
         this.firstName = userDTO.firstName();
         this.lastName = userDTO.lastName();
-        this.phoneNumber = userDTO.phoneNumber();
-        this.cpf = userDTO.cpf();
         this.email = userDTO.email();
         this.password = userDTO.password();
+        this.cpf = userDTO.cpf();
+        this.phoneNumber = userDTO.phoneNumber();
     }
 
 }
